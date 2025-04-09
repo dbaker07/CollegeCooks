72acea2cb53eece07d2ed523d9b93bbc486465d0
lucipia13 <swimdancer0113@gmail.com>
2025-04-09 07:54:31 -0400

    Firebase Added

--- app/src/main/java/com/example/collegecooks/MainActivity.java
+++ app/src/main/java/com/example/collegecooks/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.collegecooks;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
